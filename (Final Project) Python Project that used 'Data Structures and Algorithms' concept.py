008caf10cdbece2fb7f96795109013ef747907ae
Lyza-Jorella <[EMAIL]>
2023-02-18 21:28:44 +0800

    First code

---
+++ (Final Project) Python Project that used 'Data Structures and Algorithms' concept.py
@@ -0,0 +1,25 @@
+print()
+print("**********Programmed by:**********")
+print("**********Lyza Jorella R. Del Rosario**********")
+print()
+
+from tkinter import *
+import random
+
+class Snake:
+    pass
+
+class food
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
