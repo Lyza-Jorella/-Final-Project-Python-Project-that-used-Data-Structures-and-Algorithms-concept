3a51e49bf0d81c49d4a45f1e8090a879a32187a1
Lyza-Jorella <[EMAIL]>
2023-02-18 22:48:14 +0800

    codes for Snake

--- (Final Project) Python Project that used 'Data Structures and Algorithms' concept.py
+++ (Final Project) Python Project that used 'Data Structures and Algorithms' concept.py
@@ -12,14 +12,22 @@ SPEED = 50
 SPACE_SIZE = 50
 BODY_PARTS = 3
 SNAKE_COLOR = "#00FF00"
-FOOD_COLOR = "#FF1100"
+FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR = "#000000"
 
 class Snake:
-    pass
 
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
 
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
 
+        for x,y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
 
 class Food:
 
@@ -30,7 +38,7 @@ class Food:
 
         self.coordinates = [x, y]
 
-        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill= FOOD_COLOR, tag="Food")
 
 def next_turn():
     pass
