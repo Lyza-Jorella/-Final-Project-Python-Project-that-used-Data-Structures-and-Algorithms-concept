d09434e9f473cf0b110985815df2f5b732e6dd3d
Lyza-Jorella <[EMAIL]>
2023-02-18 21:43:21 +0800

    setting the game design

--- (Final Project) Python Project that used 'Data Structures and Algorithms' concept.py
+++ (Final Project) Python Project that used 'Data Structures and Algorithms' concept.py
@@ -6,6 +6,15 @@ print()
 from tkinter import *
 import random
 
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF1100"
+BACKGROUND_COLOR = "#000000"
+
 class Snake:
     pass
 
