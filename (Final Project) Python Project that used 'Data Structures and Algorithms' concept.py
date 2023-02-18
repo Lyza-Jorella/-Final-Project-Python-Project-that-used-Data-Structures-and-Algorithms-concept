6d28ca85fd93426dd37f26cc60db0811a49a0d1f
Lyza-Jorella <[EMAIL]>
2023-02-19 00:35:44 +0800

    FINAL CODE

--- (Final Project) Python Project that used 'Data Structures and Algorithms' concept.py
+++ (Final Project) Python Project that used 'Data Structures and Algorithms' concept.py
@@ -6,13 +6,13 @@ print()
 from tkinter import *
 import random
 
-GAME_WIDTH = 1000
+GAME_WIDTH = 1500
 GAME_HEIGHT = 700
 SPEED = 100
 SPACE_SIZE = 50
 BODY_PARTS = 1
-SNAKE_COLOR = "#00FF00"
-FOOD_COLOR = "#FF0000"
+SNAKE_COLOR = "#00ff00"
+FOOD_COLOR = "#e72e2e"
 BACKGROUND_COLOR = "#000000"
 
 class Snake:
@@ -54,6 +54,36 @@ def next_turn(snake, food):
     elif direction == "right":
         x += SPACE_SIZE
 
+    snake.coordinates.insert(0, (x, y))
+
+    square = canvas.create_rectangle(x,y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+
+    snake.squares.insert(0, square)
+
+    if x == food.coordinates[0] and y == food.coordinates[1]:
+
+        global score
+
+        score += 1
+
+        label.config(text="Score:{}".format(score))
+
+        canvas.delete("food")
+
+        food = Food()
+
+    else:
+        del snake.coordinates[-1]
+
+        canvas.delete(snake.squares[-1])
+
+        del snake.squares[-1]
+
+    if check_collisions(snake):
+        game_over()
+
+    else:
+        window.after(SPEED, next_turn, snake, food)
 
 
 def change_direction(new_direction):
