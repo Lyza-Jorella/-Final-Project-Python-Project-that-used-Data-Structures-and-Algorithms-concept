bc33143c91f2376715caaac2946b782ecd267a9d
Lyza-Jorella <[EMAIL]>
2023-02-18 22:38:57 +0800

    codes for Food

--- (Final Project) Python Project that used 'Data Structures and Algorithms' concept.py
+++ (Final Project) Python Project that used 'Data Structures and Algorithms' concept.py
@@ -18,8 +18,19 @@ BACKGROUND_COLOR = "#000000"
 class Snake:
     pass
 
-class food:
-    pass
+
+
+
+class Food:
+
+    def __int__(self):
+
+        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
+
+        self.coordinates = [x, y]
+
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
 
 def next_turn():
     pass
@@ -45,5 +56,21 @@ label.pack()
 
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_height()
+
+x = int ((screen_width/2) - (window_width/2))
+y = int ((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
+
 
 window.mainloop()
